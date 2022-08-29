ae25a0b469d180d32072479d1832b2e2383dcbf1
Eugene Park <epark215@gmail.com>
2022-08-29 16:15:21 +0000

    Compelted javascript-primitiaves-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,23 @@
+var fullName;
+var isCool;
+var totalPets;
+
+fullName = 'Eugene Park';
+isCool = true;
+totalPets = 1;
+
+console.log(fullName);
+console.log(isCool);
+console.log(totalPets);
+console.log(typeof fullName);
+console.log(typeof isCool);
+console.log(typeof totalPets);
+
+var unknown;
+unknown = null;
+console.log(unknown);
+console.log(typeof unknown);
+
+var never;
+console.log(never);
+console.log(typeof never);
