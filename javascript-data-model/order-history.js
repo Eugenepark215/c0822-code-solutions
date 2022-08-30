080bbb98c6f08108804f6e393502f5a9817b9080
Eugene Park <epark215@gmail.com>
2022-08-30 17:10:10 +0000

    Completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,72 @@
+var orderHistory = [
+  {
+    orderNumber: '114-3941689-8772232',
+    orderPlaced: '8/4/2020',
+    orderDelivered: '8/8/2020',
+    shipTo: 'JS Masher',
+    total: 34.00,
+    items: {
+      itemType: 'book',
+      itemImage: 'link-to-image',
+      itemName: 'JavaScript for impatient programmers',
+      author: 'Rauschmayer, Dr. Azel',
+      returnWindow: '9/7/2020',
+      itemPrice: 31.55
+    }
+  },
+  {
+    orderNumber: '113-9984268-1280257',
+    orderPlaced: '7/19/2020',
+    orderDelivered: '7/20/2020',
+    shipTo: 'JS Masher',
+    total: 44.53,
+    items: {
+      itemType: 'book',
+      itemImage: 'link-to-image',
+      itemName: 'The Timeless Way of Building',
+      author: 'Alexander, Christopher',
+      returnWindow: '8/19/2020',
+      itemPrice: 41.33
+    }
+  },
+  {
+    orderNumber: '114-287557-9059409',
+    orderPlaced: '7/4/2020',
+    orderDelivered: '7/7/2020',
+    shipTo: 'JS Masher',
+    total: 17.22,
+    items: [
+      {
+        itemType: 'electronic',
+        itemImage: 'link-to-image',
+        itemName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        returnWindow: '8/5/2020',
+        itemPrice: 15.98
+      }
+    ]
+  },
+  {
+    orderNumber: '113-2883177-2648248',
+    orderPlaced: '7/3/2020',
+    orderDelivered: '7/5/2020',
+    shipTo: 'JS Masher',
+    total: 138.93,
+    items: [
+      {
+        itemType: 'electronic',
+        itemImage: 'link-to-image',
+        itemName: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        returnWindow: '8/4/2020',
+        itemPrice: 94.95
+      },
+      {
+        itemType: 'book',
+        itemImage: 'link-to-image',
+        itemName: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        returnWindow: '8/4/2020',
+        itemPrice: 33.99
+      }
+    ]
+  }
+];
