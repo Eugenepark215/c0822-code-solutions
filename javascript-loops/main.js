864fa404206dedeaac423f88fea5fdccf1f022fc
Eugene Park <epark215@gmail.com>
2022-08-31 20:44:07 +0000

    Completed javascript-loop

---
+++ javascript-loops/main.js
@@ -0,0 +1,62 @@
+/* exported getNumbersToTen,
+            getEvenNumbersToTwenty,
+            repeatWord,
+            logEachCharacter,
+            doubleAll,
+            getKeys,
+            getValues
+ */
+
+function getNumbersToTen() {
+  var currentNumber = 1;
+  var numbers = [];
+  while (currentNumber < 11) {
+    numbers.push(currentNumber);
+    currentNumber++;
+  } return numbers;
+}
+
+function getEvenNumbersToTwenty() {
+  var evenNumber = [];
+  var currentNumber = 2;
+  while (currentNumber < 21) {
+    evenNumber.push(currentNumber);
+    currentNumber += 2;
+  } return evenNumber;
+}
+
+function repeatWord(word, times) {
+  var count = 1;
+  var repeated = '';
+  while (count < times) {
+    repeated += word;
+    count++;
+  } return repeated;
+}
+
+function logEachCharacter(string) {
+  for (var i = 0; i < string.length; i++) {
+    console.log(string[i]);
+  }
+}
+
+function doubleAll(numbers) {
+  var doulbed = [];
+  for (var i = 0; i < numbers.length; i++) {
+    doulbed.push((numbers[i]) * 2);
+  } return doulbed;
+}
+
+function getKeys(object) {
+  var keys = [];
+  for (var x in object) {
+    keys.push(x);
+  } return keys;
+}
+
+function getValues(object) {
+  var values = [];
+  for (var x in object) {
+    values.push(object[x]);
+  } return values;
+}
