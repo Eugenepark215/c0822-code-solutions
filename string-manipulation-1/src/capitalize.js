193d5d67341a381aa73824f0e43b99528c3d0be7
Eugene Park <epark215@gmail.com>
2022-09-01 22:35:56 +0000

    In progress

---
+++ string-manipulation-1/src/capitalize.js
@@ -0,0 +1,9 @@
+/* exported capitalize */
+// word is entered and it's first letter is capitalized and rest lower case
+// string.length[0] = beginning applied uppercase() rest lowercase()
+// new string returned string[] need to be added into empty string variable
+// loop to transform each letter in the string
+// loop with length of string
+
+function capitalize(word) {
+}
