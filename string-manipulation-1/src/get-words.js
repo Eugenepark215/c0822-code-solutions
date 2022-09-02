e8373b723862136f988fed68344b71699a5da810
Eugene Park <epark215@gmail.com>
2022-09-02 21:04:45 +0000

    Completed string-manipulation-1

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,1 +1,20 @@
 /* exported getWords */
+// string input that can contain 0 and spaces
+// return array contained with each word in string
+// split words from the spaces
+// split makes array
+// for loop to go through the string
+// if else to check to see if charater is space or not
+// if there's space in string split and return
+// if it doesn't return as is
+// no value return empty array
+function getWords(string) {
+  var getWordsEmptyArray = [];
+  for (var i = 0; i < string.length; i++) {
+    if (string[i] === ' ') {
+      return string.split(' ');
+    } else if (string.includes(' ') === false) {
+      return [string];
+    }
+  } return getWordsEmptyArray;
+}
