de10e2bec5c28d3357c4447ef6dd284e7a23de08
Eugene Park <epark215@gmail.com>
2022-09-06 17:06:15 +0000

    Completed dom-querying

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -1,9 +1,9 @@
 console.log('hello, world');
 console.log(document.querySelector('h1'));
-console.dir('h1');
+console.dir(document.querySelector('h1'));
 console.log(document.querySelector('#explanation'));
-console.dir('#explantaion');
+console.dir(document.querySelector('#explanation'));
 console.log(document.querySelector('.hint'));
-console.dir('.hint');
+console.dir(document.querySelector('.hint'));
 console.log(document.querySelector('p'));
 console.log(document.querySelectorAll('.example-link'));
