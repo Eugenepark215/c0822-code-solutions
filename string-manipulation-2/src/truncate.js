e518d7a27cac140986b824587c21fd3a83551932
Eugene Park <epark215@gmail.com>
2022-09-08 03:49:38 +0000

    In progress

--- string-manipulation-2/src/truncate.js
+++ string-manipulation-2/src/truncate.js
@@ -4,11 +4,18 @@
 // string input is cut down to the length and appends ...
 // loop that concludes at given length
 // add '...' at the end
+// if else to check empty & string.length < length
 
 function truncate(length, string) {
   var object = '';
   for (var i = 0; i < length; i++) {
-    object += string[i];
+    if (string.length === 0) {
+      return '...';
+    } else if (string.length < length) {
+      return string + '...';
+    } else {
+      object += string[i];
+    }
   }
   return object + '...';
 }
