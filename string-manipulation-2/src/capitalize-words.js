1dd3cec26d64fe7554cef8b1f96da3b63c438bb1
Eugene Park <epark215@gmail.com>
2022-09-08 21:06:27 +0000

    In progress

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -2,3 +2,7 @@
 // string parameter
 // first letter is Capitalized and rest is lowercased
 // JavaScript is always JavaScript
+// for loop to go through string
+// capitalize(word[1]) + lowercase(word[i])
+// unshfit for word[1]
+// will check contain()
