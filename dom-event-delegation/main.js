28d219d0fac8adc910f83630b3fb4b2b5f920faf
Eugene Park <epark215@gmail.com>
2022-09-08 17:08:16 +0000

    Revised into single event listener

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -1,10 +1,8 @@
-function callBack(event) {
+document.querySelector('.task-list').addEventListener('click', function (event) {
+
   console.log(event.target);
   console.log(event.target.tagName);
-}
-document.querySelector('.task-list').addEventListener('click', callBack);
 
-document.querySelector('.task-list').addEventListener('click', function (event) {
   if (event.target.tagName === 'BUTTON') {
     console.log(event.target.closest('.task-list-item'));
     event.target.closest('.task-list-item').remove();
