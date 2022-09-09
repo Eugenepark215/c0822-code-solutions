bb5ed720a0703be66613c10badff551398a793a4
Eugene Park <epark215@gmail.com>
2022-09-09 04:11:33 +0000

    In progress

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,1 +1,13 @@
 /* exported capitalizeWord */
+// string parameter
+// first letter is Capitalized and rest is lowercased
+// JavaScript is always JavaScript
+// for loop to go through string
+// capitalize(word[1]) + lowercase(word[i])
+// unshfit for word[1]
+// will check contain()
+function capitalizeWord(word) {
+  for (var i = 0; i < word.length; i++) {
+    word[0].toUpperCase();
+  }
+}
--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,8 +1,1 @@
 /* exported capitalizeWords */
-// string parameter
-// first letter is Capitalized and rest is lowercased
-// JavaScript is always JavaScript
-// for loop to go through string
-// capitalize(word[1]) + lowercase(word[i])
-// unshfit for word[1]
-// will check contain()
