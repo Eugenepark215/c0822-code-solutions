f1f1bbb42ad5831f758dc632bf1ae489bddb9bab
Eugene Park <epark215@gmail.com>
2022-09-30 23:47:34 +0000

    Completed array-manipulation-3

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -3,3 +3,21 @@
 // given the values at the postion pair the two values into an array
 // push that array into new array
 // if there are no pair push into array by itself
+function zip(first, second) {
+  var newArray = [];
+  var newArrayArray = [];
+  if (first.length > second.length) {
+    for (var i = 0; i < second.length; i++) {
+      newArrayArray.push(first[i]);
+      newArrayArray.push(second[i]);
+      newArray.push(newArrayArray);
+    }
+  } if (first.length < second.length) {
+    for (i = 0; i < first.length; i++) {
+      newArrayArray.push(first[i]);
+      newArrayArray.push(second[i]);
+      newArray.push(newArrayArray);
+    }
+  }
+  return newArray;
+}
