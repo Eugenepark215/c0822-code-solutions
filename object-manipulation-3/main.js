baf2c77045626f55b5ecf933c96901d67db592dd
Eugene Park <epark215@gmail.com>
2022-10-05 21:33:02 +0000

    In progress

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -7,3 +7,41 @@ console.log('Lodash is loaded:', typeof _ !== 'undefined');
 // shuffle deck
 // deal 2 cards to each player no players should be able to recieve the same card
 // find player with highest score
+// Ace = 11 face = 10 number = number
+// create array with 4 objects with 2 prop name hand
+// create array with 52 object
+// random method
+// if player is assigned a card remove obejct from 52 object
+// access player hand property and add value
+
+// var players = [
+//   {
+//     name: 'One',
+//     hand: ''
+//   },
+//   {
+//     name: 'Two',
+//     hand: ''
+//   },
+//   {
+//     name: 'Three',
+//     hand: ''
+//   },
+//   {
+//     name: 'Four',
+//     hand: ''
+//   }
+// ];
+
+// function createDeck() {
+//   var deck = [];
+//   var card = {};
+//   for (var i = 0; i <= 52; i++) {
+//     if (i === 0) {
+//       card.suit = 'clubs';
+//       card.rank = 'Ace';
+//       deck.push(card);
+//     }
+//   }
+//   return deck;
+// }
