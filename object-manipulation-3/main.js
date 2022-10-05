e86640a78552d10543abb84a086dc0259c48b05d
Eugene Park <epark215@gmail.com>
2022-10-05 17:05:00 +0000

    In progress

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,9 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+// create collection of four objects
+// each object has a unique property name and hand
+// create an object that represents a 52 card deck
+// each card should have rank (Ace, 2, 3, 4, 5 ... King)
+// each card should have a suit (club, spade, heart, diamonds)
+// shuffle deck
+// deal 2 cards to each player no players should be able to recieve the same card
+// find player with highest score
