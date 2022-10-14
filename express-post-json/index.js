f72731c976bb2d567d9244fff43fa00c3321a57c
Eugene Park <epark215@gmail.com>
2022-10-14 23:21:45 +0000

    Completed express-post-json

---
+++ express-post-json/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const grades = {};
+var nextId = 1;
+
+app.get('/api/grades', (req, res) => {
+  var array = [];
+  for (const x in grades) {
+    array.push(grades[x]);
+  }
+  res.json(array);
+});
+
+app.use(express.json());
+
+app.post('/api/grades', (req, res) => {
+  grades[nextId] = req.body;
+  grades[nextId].id = nextId;
+  nextId++;
+  res.sendStatus(201);
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Express server 3000 is listening!');
+});
