8f6ea1d75eb78fc66c942a4796a2b1fb971d6ee2
Eugene Park <epark215@gmail.com>
2022-10-16 02:20:38 +0000

    Included fs to edit data.json

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var app = express();
+const fs = require('fs');
 const dataJSON = require('./data.json');
 const error = require('./error.json');
 var nextId = dataJSON.nextId;
@@ -32,7 +33,35 @@ app.post('/api/notes', (req, res) => {
     dataJSON.notes[nextId] = req.body;
     dataJSON.notes[nextId].id = nextId;
     nextId++;
-    res.status(201).json(req.body);
+    dataJSON.nextId++;
+    fs.writeFile('data.json', JSON.stringify(dataJSON, null, 2), err => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      res.status(201).json(req.body);
+    });
+  } else {
+    console.error(error.unexpectedError);
+    res.sendStatus(500);
+  }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  if (parseInt(req.params.id) < 0 || isNaN(parseInt(req.params.id))) {
+    res.status(400).json(error.positiveInteger);
+  } else if (parseInt(req.params.id) > 0 && dataJSON.notes[req.params.id] === undefined) {
+    error.cannotFindId = { error: 'cannot find note with id ' + req.params.id };
+    res.status(404).json(error.cannotFindId);
+  } else if (parseInt(req.params.id) > 0 && dataJSON.notes[req.params.id] !== undefined) {
+    delete dataJSON.notes[req.params.id];
+    fs.writeFile('data.json', JSON.stringify(dataJSON, null, 2), err => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      res.sendStatus(204);
+    });
   } else {
     console.error(error.unexpectedError);
     res.sendStatus(500);
