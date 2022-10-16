40850950ca6c4aa6800b756f788931179068721b
Eugene Park <epark215@gmail.com>
2022-10-16 01:44:11 +0000

    Completed clients can POST a new note

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,27 +1,41 @@
 const express = require('express');
 var app = express();
 const dataJSON = require('./data.json');
-const notes = [];
 const error = require('./error.json');
-// var nextId = 1;
+var nextId = dataJSON.nextId;
 
 app.use(express.json());
 
 app.get('/api/notes', (req, res) => {
+  var array = [];
   for (const x in dataJSON.notes) {
-    notes.push(dataJSON.notes[x]);
+    array.push(dataJSON.notes[x]);
   }
-  res.status(200).json(notes);
+  res.status(200).json(array);
 });
 
 app.get('/api/notes/:id', (req, res) => {
   if (parseInt(req.params.id) > 0 && dataJSON.notes[req.params.id] === undefined) {
-    error[404] = { error: 'cannot find note with id ' + req.params.id };
-    res.status(404).json(error[404]);
+    error.cannotFindId = { error: 'cannot find note with id ' + req.params.id };
+    res.status(404).json(error.cannotFindId);
   } else if (parseInt(req.params.id) > 0 && dataJSON.notes[req.params.id] !== undefined) {
     res.status(200).json(dataJSON.notes[req.params.id]);
-  } else if (isNaN(parseInt(req.params.id)) || parseInt(req.params.id) < 0) {
-    res.status(400).json(error[400]);
+  } else {
+    res.status(400).json(error.positiveInteger);
+  }
+});
+
+app.post('/api/notes', (req, res) => {
+  if (req.body.content === undefined) {
+    res.status(400).json(error.contentRequired);
+  } else if (req.body.content !== undefined) {
+    dataJSON.notes[nextId] = req.body;
+    dataJSON.notes[nextId].id = nextId;
+    nextId++;
+    res.status(201).json(req.body);
+  } else {
+    console.error(error.unexpectedError);
+    res.sendStatus(500);
   }
 });
 
