7cb42ee07c97ec65169ec78edab2757407b60051
Eugene Park <epark215@gmail.com>
2022-10-19 22:43:43 +0000

    Completed sgt-back-end

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -93,6 +93,39 @@ app.put('/api/grades/:gradeId', (req, res) => {
     });
 });
 
+app.delete('/api/grades/:gradeId', (req, res) => {
+  const gradeId = Number(req.params.gradeId);
+  if (req.params.gradeId === undefined || req.params.gradeId > 100 || req.params.gradeId < 0 || isNaN(req.params.gradeId)) {
+    res.status(400).json({
+      error: 'Invalid gradeId'
+    });
+    return;
+  }
+  const sql = `
+  delete from "grades"
+  where "gradeId" = $1
+  returning *
+  `;
+  const param = [req.params.gradeId];
+  db.query(sql, param)
+    .then(result => {
+      const grade = result.rows[0];
+      if (!grade) {
+        res.status(404).json({
+          error: `Cannot find grade with gradeId ${gradeId}`
+        });
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
+});
+
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Express server listening on port 3000');
