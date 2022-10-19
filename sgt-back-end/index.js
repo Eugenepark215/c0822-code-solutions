fed82dfad3283a1a644e8714975cab2222c6616d
Eugene Park <epark215@gmail.com>
2022-10-19 21:21:19 +0000

    Completed post/api/grades

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -1,6 +1,7 @@
 const pg = require('pg');
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 const db = new pg.Pool({
   connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
@@ -26,6 +27,31 @@ app.get('/api/grades', (req, res) => {
       console.error(err);
       res.status(500).json({
         error: 'An unexpected error occurred.'
+      });
+    });
+});
+
+app.post('/api/grades/', (req, res) => {
+  if (req.body === undefined || req.body.course === undefined || req.body.name === undefined || req.body.score === undefined || req.body.score < 0 || req.body.score > 100) {
+    res.status(400).json({
+      error: 'Invalid content'
+    });
+  }
+  const sql = `
+  insert into "grades" ("course", "name", "score")
+  values ($1, $2, $3)
+  returning *
+  `;
+  const params = [req.body.course, req.body.name, req.body.score];
+  db.query(sql, params)
+    .then(result => {
+      res.status(201).json(result.rows[0]);
+    }
+    )
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occured.'
       });
     });
 });
