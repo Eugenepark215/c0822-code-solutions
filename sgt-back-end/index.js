1de2ee082c0d99903ad44ee74c273afcb7979397
Eugene Park <epark215@gmail.com>
2022-10-19 22:26:53 +0000

    Completed put/api/grades/:gradeid

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -36,6 +36,7 @@ app.post('/api/grades/', (req, res) => {
     res.status(400).json({
       error: 'Invalid content'
     });
+    return;
   }
   const sql = `
   insert into "grades" ("course", "name", "score")
@@ -52,6 +53,42 @@ app.post('/api/grades/', (req, res) => {
       console.error(err);
       res.status(500).json({
         error: 'An unexpected error occured.'
+      });
+    });
+});
+
+app.put('/api/grades/:gradeId', (req, res) => {
+  const gradeId = Number(req.params.gradeId);
+  if (req.body === undefined || req.body.course === undefined || req.body.name === undefined || req.body.score === undefined || req.body.score < 0 || req.body.score > 100) {
+    res.status(400).json({
+      error: 'Invalid content'
+    });
+    return;
+  }
+  const sql = `
+    update "grades"
+    set "course" = $1,
+    "name" = $2,
+    "score" = $3
+    where "gradeId" = $4
+    returning *
+    `;
+  const params = [req.body.course, req.body.name, req.body.score, gradeId];
+  db.query(sql, params)
+    .then(result => {
+      const grade = result.rows[0];
+      if (!grade) {
+        res.status(404).json({
+          error: `Cannot find grade with gradeId ${gradeId}`
+        });
+      } else {
+        res.status(200).json(grade);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
       });
     });
 });
