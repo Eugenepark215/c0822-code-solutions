ffb619e89bbe29e5845238c340106429d29bcdbb
Eugene Park <epark215@gmail.com>
2022-10-27 21:21:28 +0000

    Revised response to res

--- fetch/main.js
+++ fetch/main.js
@@ -1,9 +1,9 @@
 fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json())
+  .then(res => res.json())
   .then(data => console.log(data))
   .catch(err => console.error('Fetch failed', err));
 
 fetch('https://pokeapi.co/api/v2/pokemon/7')
-  .then(response => response.json())
+  .then(res => res.json())
   .then(data => console.log(data))
   .catch(err => console.error('Fetch failed', err));
