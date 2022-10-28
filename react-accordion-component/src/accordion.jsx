5075742c46170e21f3f7b0dc47f730103f831c79
Eugene Park <epark215@gmail.com>
2022-10-28 20:44:49 +0000

    refactored into class component

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,17 +1,47 @@
-// import React from 'react';
+import React from 'react';
 
-// const languages = [
-//   { topic: 'Hypertext Markup Language', details: 'Hypertext Markup Language (HTML)' },
-//   { topic: 'Cascading Style Sheets', details: 'Cascading Style Sheets (CSS)' },
-//   { topic: 'JavaScript', details: 'JavaScript, often abbreviated as JS,' }
-// ];
+export default class Accordian extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      isClicked: false
+    };
+  }
 
-// class Accordian extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.handleClick = this.handleClick.bind(this);
-//     this.state = {
-//       isClick: false
-//     };
-//   }
-// }
+  handleClick() {
+    if (!this.state.isClicked) {
+      this.setState({ isClicked: true });
+    } else if (this.state.isClicked) {
+      this.setState({ isClicked: false });
+    }
+  }
+
+  render() {
+    const languages = [
+      { topic: 'Hypertext Markup Language', details: 'Hypertext Markup Language (HTML)' },
+      { topic: 'Cascading Style Sheets', details: 'Cascading Style Sheets (CSS)' },
+      { topic: 'JavaScript', details: 'JavaScript, often abbreviated as JS,' }
+    ];
+    if (!this.state.isClicked) {
+      return (
+  <ul>
+    {languages.map(languages =>
+      <li className ='topic' key={languages.topic} onClick={this.handleClick}>{languages.topic}
+      </li>
+    )}
+  </ul>);
+    } else if (this.state.isClicked) {
+      return (
+  <ul>
+    {languages.map(languages =>
+      <li className ='topic' key={languages.topic} onClick={this.handleClick}>{languages.topic}
+        <li className='details' onClick={this.handleClick}>{languages.details}</li>
+      </li>
+    )}
+  </ul>);
+    }
+  }
+}
+
+// need to target the key
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -1,20 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Accordian from './accordion';
 
-const languages = [
-  { topic: 'Hypertext Markup Language', details: 'Hypertext Markup Language (HTML)' },
-  { topic: 'Cascading Style Sheets', details: 'Cascading Style Sheets (CSS)' },
-  { topic: 'JavaScript', details: 'JavaScript, often abbreviated as JS,' }
-];
-const languageTopics = (
-  <ul>
-    {languages.map(languages =>
-      <li className ='topic' key={languages.topic}>{languages.topic}
-        <li className='details'>{languages.details}</li>
-      </li>
-    )}
-  </ul>
-);
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
-root.render(languageTopics);
+root.render(<Accordian />);
