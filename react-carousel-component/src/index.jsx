20cebad824d20ade431ba8e83662766f338d6013
Eugene Park <epark215@gmail.com>
2022-11-01 16:04:50 +0000

    Set up

---
+++ react-carousel-component/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Carousel from './carousel.jsx';
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+root.render(<Carousel/>);
