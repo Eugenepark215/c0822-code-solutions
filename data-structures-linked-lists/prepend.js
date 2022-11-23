3b43a0129eccb6160afb65ac9fab4e5184722e6e
Eugene Park <epark215@gmail.com>
2022-11-23 20:56:46 +0000

    Refactored prepend

--- data-structures-linked-lists/prepend.js
+++ data-structures-linked-lists/prepend.js
@@ -2,8 +2,7 @@
 /* exported prepend */
 
 function prepend(list, value) {
-  const newList = new LinkedList(value);
   const copyList = list;
-  const merged = new LinkedList(newList.data, copyList.data, copyList.next.data, copyList.next.next.data, copyList.next.next.next.data);
-  return merged;
+  const newList = new LinkedList(value, copyList.data, copyList.next.data, copyList.next.next.data, copyList.next.next.next.data);
+  return newList;
 }
