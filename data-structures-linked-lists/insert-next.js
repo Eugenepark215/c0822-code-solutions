5c21d3951a0e00d1b8ff7d8bf6555b64b6d263b5
Eugene Park <epark215@gmail.com>
2022-11-23 20:54:16 +0000

    Refactored insert-next

--- data-structures-linked-lists/insert-next.js
+++ data-structures-linked-lists/insert-next.js
@@ -2,11 +2,8 @@
 /* exported insertNext */
 
 function insertNext(list, value) {
-  const old1 = list.next.data;
-  const old2 = list.next.next.data;
-  const old3 = list.next.next.next.data;
-  list.next.data = value;
-  list.next.next.data = old1;
-  list.next.next.next.data = old2;
-  list.next.next.next.next = new LinkedList(old3);
+  const newList = new LinkedList(value);
+  const otherList = new LinkedList(list.next.data, list.next.next.data, list.next.next.next.data);
+  list.next = newList;
+  list.next.next = otherList;
 }
