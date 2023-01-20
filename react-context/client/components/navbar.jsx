b373ce82a4a911e694e29931438acbfb33cc42c9
Eugene Park <epark215@gmail.com>
2023-01-20 04:34:43 +0000

    removed comment

--- react-context/client/components/navbar.jsx
+++ react-context/client/components/navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-// import React from 'react';
 import AppContext from '../lib/app-context';
 
 export default function Navbar() {
@@ -33,39 +32,5 @@ export default function Navbar() {
       </nav>
   );
 }
-
-// export default class Navbar extends React.Component {
-//   render() {
-//     const { user, handleSignOut } = this.context;
-//     return (
-//       <nav className="navbar navbar-dark bg-dark">
-//         <div className="container">
-//           <a className="navbar-brand" href="#">
-//             <i className="fas fa-bolt me-2" />
-//             Awesome App
-//           </a>
-//           <div>
-//             { user !== null &&
-//               <button className="btn btn-dark" onClick={handleSignOut}>
-//                 Sign out
-//                 <i className="ms-2 fas fa-sign-out-alt" />
-//               </button>
-//             }
-//             { user === null &&
-//               <>
-//                 <a href="#sign-in" className="btn btn-primary">
-//                   Sign In
-//                 </a>
-//                 <a href="#sign-up" className="btn btn-dark">
-//                   Sign Up
-//                 </a>
-//               </>
-//             }
-//           </div>
-//         </div>
-//       </nav>
-//     );
-//   }
-// }
 
 Navbar.contextType = AppContext;
