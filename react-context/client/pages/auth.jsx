133fa1a747bddb134d5e666337ba0659dd2cf60e
Eugene Park <epark215@gmail.com>
2023-01-20 04:40:03 +0000

    Refacted AuthPage useContext

--- react-context/client/pages/auth.jsx
+++ react-context/client/pages/auth.jsx
@@ -1,19 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Redirect from '../components/redirect';
 import AuthForm from '../components/auth-form';
 import AppContext from '../lib/app-context';
 
-export default class AuthPage extends React.Component {
-  render() {
+export default function AuthPage() {
+  const context = useContext(AppContext);
 
-    const { user, route, handleSignIn } = this.context;
-
-    if (user) return <Redirect to="" />;
-
-    const welcomeMessage = route.path === 'sign-in'
-      ? 'Please sign in to continue'
-      : 'Create an account to get started!';
-    return (
+  if (context.user) return <Redirect to="" />;
+  const welcomeMessage = context.route.path === 'sign-in'
+    ? 'Please sign in to continue'
+    : 'Create an account to get started!';
+  return (
       <div className="row pt-5 align-items-center">
         <div className="col-12 offset-0 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-xl-4 offset-xl-4">
           <header className="text-center">
@@ -25,14 +22,13 @@ export default class AuthPage extends React.Component {
           </header>
           <div className="card p-3 ">
             <AuthForm
-              key={route.path}
-              action={route.path}
-              onSignIn={handleSignIn} />
+              key={context.route.path}
+              action={context.route.path}
+              onSignIn={context.handleSignIn} />
           </div>
         </div>
       </div>
-    );
-  }
+  );
 }
 
 AuthPage.contextType = AppContext;
